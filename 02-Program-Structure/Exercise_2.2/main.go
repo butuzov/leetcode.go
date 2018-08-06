ec1b5401cb8e2193e6eddc49bca89e6232915c07
Oleg Butuzov <butuzov@made.ua>
2018-08-06 13:01:44 +0300

    final solution

--- 02-Program-Structure/Exercise_2.2/main.go
+++ 02-Program-Structure/Exercise_2.2/main.go
@@ -1,19 +1,71 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 
 	tc "./tempconv"
 )
 
+// Cumulative arguments.
+
+// Flags arguments.
+var temperature = flag.Float64("t", 0, "A Feed Token from TeemTreeHouse.com")
+var scale = flag.String("s", "Celsius", "Temperature Scale can be Celsius, Fahrenheit or Kalvin")
+
 func main() {
-	fmt.Println("This code converts temperature in Celcius/Fahrenheiht/Kalvin (e.g.)")
-	fmt.Printf("%5s -> %9s \n", tc.Celsius(0), tc.CtoF(tc.Celsius(10)))
-	fmt.Printf("%5s -> %9s \n", tc.Celsius(10), tc.CtoK(tc.Celsius(10)))
+	flag.Parse()
 
-	fmt.Printf("%5s -> %9s \n", tc.Kalvin(10), tc.KtoC(tc.Kalvin(10)))
-	fmt.Printf("%5s -> %9s \n", tc.Kalvin(0), tc.KtoF(tc.Kalvin(0)))
+	var errorBelowZeroMsg = "Provided temperature %s below absolute zero point\n"
+	var reportMsg = "%s is %s or %s\n"
 
-	fmt.Printf("%5s -> %9s \n", tc.Fahrenheit(10), tc.FtoC(tc.Fahrenheit(10)))
-	fmt.Printf("%5s -> %9s \n", tc.Fahrenheit(0), tc.FtoK(tc.Fahrenheit(0)))
+	fmt.Printf("You have entered: %0.2f °%s\n", *temperature, *scale)
+
+	switch *scale {
+	case "Celsius":
+
+		degrees := tc.Celsius(*temperature)
+
+		if degrees < tc.AbsoluteZeroC {
+			log.Fatalf(errorBelowZeroMsg, degrees)
+		}
+
+		fmt.Printf(reportMsg, degrees, tc.CtoF(degrees), tc.CtoK(degrees))
+
+		break
+	case "Fahrenheit":
+
+		degrees := tc.Fahrenheit(*temperature)
+
+		if degrees < tc.AbsoluteZeroF {
+			log.Fatalf(errorBelowZeroMsg, degrees)
+		}
+
+		fmt.Printf(reportMsg, degrees, tc.FtoC(degrees), tc.FtoK(degrees))
+
+		break
+	case "Kalvin":
+
+		degrees := tc.Kalvin(*temperature)
+
+		if degrees < tc.AbsoluteZeroK {
+			log.Fatalf(errorBelowZeroMsg, degrees)
+		}
+
+		fmt.Printf(reportMsg, degrees, tc.KtoF(degrees), tc.KtoC(degrees))
+
+		break
+
+	}
+
+	// fmt.Println("This code converts temperature in Celcius/Fahrenheiht/Kalvin (e.g.)")
+	// fmt.Printf("%5s -> %9s \n", tc.Celsius(0), tc.CtoF(tc.Celsius(10)))
+	// fmt.Printf("%5s -> %9s \n", tc.Celsius(10), tc.CtoK(tc.Celsius(10)))
+
+	// fmt.Printf("%5s -> %9s \n", tc.Kalvin(10), tc.KtoC(tc.Kalvin(10)))
+	// fmt.Printf("%5s -> %9s \n", tc.Kalvin(0), tc.KtoF(tc.Kalvin(0)))
+
+	// fmt.Printf("%5s -> %9s \n", tc.Fahrenheit(10), tc.FtoC(tc.Fahrenheit(10)))
+	// fmt.Printf("%5s -> %9s \n", tc.Fahrenheit(0), tc.FtoK(tc.Fahrenheit(0)))
 }
---
+++ 02-Program-Structure/Exercise_2.2/readme.md
@@ -0,0 +1,9 @@
+# CLI for `tempconv`
+
+```bash
+go build -o cf
+./cf --help
+./cf -s=Fahrenheit -t=32
+./cf -s=Celsius -t=-500
+./cf -s=Kalvin -t=400
+```
